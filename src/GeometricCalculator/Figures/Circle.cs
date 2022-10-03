eefefe3aa69e25b25e1a1515e7cf98a8047c8b72
Drombeys <dima.zolotov02@gmail.com>
2022-10-03 19:58:24 +0300

    Add circle figure

---
+++ src/GeometricCalculator/Figures/Circle.cs
@@ -0,0 +1,23 @@
+﻿namespace GeometricCalculator.Figures;
+
+/// <summary>
+/// Geometric figure circle
+/// </summary>
+public class Circle
+{
+    /// <summary>
+    /// Radius
+    /// </summary>
+    public double Radius { get; }
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="radius">Circle radius</param>
+    public Circle(double radius)
+    {
+        ExceptionHelpers.ThrowArgumentOutOfRange(radius <= 0, "Circle radius must be greater than 0");
+
+        Radius = radius;
+    }
+}
