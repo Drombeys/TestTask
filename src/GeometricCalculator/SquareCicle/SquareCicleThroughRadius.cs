75999ac1a3e6331fe568093035dc3e63f5cdec54
Drombeys <dima.zolotov02@gmail.com>
2022-10-04 01:42:16 +0300

    Implement calculate square cicle through radius

---
+++ src/GeometricCalculator/SquareCicle/SquareCicleThroughRadius.cs
@@ -0,0 +1,16 @@
+﻿using GeometricCalculator.Figures;
+
+namespace GeometricCalculator.SquareTriangle;
+
+/// <summary>
+/// Implementation calculate square cicle through radius
+/// </summary>
+public sealed class SquareCicleThroughRadius : ISquare<Circle>
+{
+    /// <summary>
+    /// Calculate square cicle through radius
+    /// </summary>
+    /// <param name="figure">Geometric figure</param>
+    /// <returns>Square</returns>
+    public double Square(Circle figure) => Math.PI * Math.Pow(figure.Radius, 2);
+}
