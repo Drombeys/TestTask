df94197f1a2e1c99b0b305effc2db0b6102ac4d5
Drombeys <dima.zolotov02@gmail.com>
2022-10-04 01:58:36 +0300

    Add test for `SquareCicleThroughRadius`

---
+++ src/GeometricCalculator.Tests/SquareCicle/SquareCicleThroughRadiusTest.cs
@@ -0,0 +1,14 @@
+﻿namespace GeometricCalculator.Tests.SquareCicle;
+
+[TestClass]
+public class SquareCicleThroughRadiusTest
+{
+    [TestMethod]
+    public void Square_Cicle_ThroughRadius_Result_6()
+    {
+        ISquare<Circle> square = new SquareCicleThroughRadius();
+        var result = square.Square(new Circle(4));
+
+        Assert.AreEqual(50.3, Math.Round(result, 1));
+    }
+}
--- src/GeometricCalculator.Tests/Usings.cs
+++ src/GeometricCalculator.Tests/Usings.cs
@@ -1,2 +1,3 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using GeometricCalculator.Figures;
+global using GeometricCalculator.SquareTriangle;
