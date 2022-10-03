44345c4a288571a684229a2a9da780c94d9eafd1
Drombeys <dima.zolotov02@gmail.com>
2022-10-03 20:01:44 +0300

    Add test for `Circle`

---
+++ src/GeometricCalculator.Tests/Figures/CircleTest.cs
@@ -0,0 +1,12 @@
+﻿namespace GeometricCalculator.Tests.Figures;
+
+[TestClass]
+public class CircleTest
+{
+    [TestMethod]
+    public void RadiusGreaterThan_0_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Circle(0));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Circle(-5));
+    }
+}
