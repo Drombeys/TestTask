d9d3577c61c0e9f0252690f662d3d1f088029d42
Drombeys <dima.zolotov02@gmail.com>
2022-10-03 19:35:40 +0300

    Add tests for `Triangle`

---
+++ src/PerimeterCalculation.Tests/Figures/TriangleTest.cs
@@ -0,0 +1,25 @@
+﻿namespace PerimeterCalculation.Tests.Figures;
+
+[TestClass]
+public class TriangleTest
+{
+    [TestMethod]
+    public void IsRectangular_Result_True_Test1()
+    {
+        var triangle = new Triangle(13, 12, 5);
+        Assert.IsTrue(triangle.IsRectangular());
+    }
+
+    [TestMethod]
+    public void IsRectangular_Result_False_Test2()
+    {
+        var triangle = new Triangle(20, 20, 20);
+        Assert.IsFalse(triangle.IsRectangular());
+    }
+
+    [TestMethod]
+    public void SideGreaterThan_0_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Triangle(0, 20, 20));
+    }
+}
