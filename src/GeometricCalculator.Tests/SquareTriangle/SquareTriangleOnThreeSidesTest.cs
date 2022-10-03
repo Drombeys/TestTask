1af5a01f9f23caa9bc4616121338369d5b3d5f76
Drombeys <dima.zolotov02@gmail.com>
2022-10-04 01:38:39 +0300

    Add tests for `SquareTriangleOnThreeSides`

---
+++ src/GeometricCalculator.Tests/SquareTriangle/SquareTriangleOnThreeSidesTest.cs
@@ -0,0 +1,19 @@
+﻿using GeometricCalculator.SquareTriangle;
+
+namespace GeometricCalculator.Tests.SquareTriangle;
+
+[TestClass]
+public class SquareTriangleOnThreeSidesTest
+{
+    [TestMethod]
+    public void Square_Triangle_Result_6()
+    {
+        var triangle = new Triangle(3, 4, 5);
+
+        IPerimeter<Triangle> perimeter = new PerimeterTriangle();
+        ISquare<Triangle> square = new SquareTriangleOnThreeSides(perimeter);
+        var result = square.Square(triangle);
+
+        Assert.AreEqual(result, 6.0f, $"Square = {result}");
+    }
+}
