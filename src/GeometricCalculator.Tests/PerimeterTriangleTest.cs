b2c992b984bc636868a033c6bec6aa51ea75c2f8
Drombeys <dima.zolotov02@gmail.com>
2022-10-04 01:38:04 +0300

    Add tests for `PerimeterTriangle`

---
+++ src/GeometricCalculator.Tests/PerimeterTriangleTest.cs
@@ -0,0 +1,14 @@
+﻿namespace GeometricCalculator.Tests;
+
+[TestClass]
+public class PerimeterTriangleTest
+{
+    [TestMethod]
+    public void Perimeter_Triangle_Result_38()
+    {
+        IPerimeter<Triangle> perimeter = new PerimeterTriangle();
+        var result = perimeter.Perimeter(new Triangle(13, 20, 5));
+
+        Assert.AreEqual(result, 38.0f, $"Perimeter = {result}");
+    }
+}
