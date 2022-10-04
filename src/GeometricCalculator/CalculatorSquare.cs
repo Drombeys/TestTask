9363c17857b0e0f326bf41119ade3c98b5696944
Drombeys <dima.zolotov02@gmail.com>
2022-10-04 15:02:37 +0300

    Implement unified calculate square figures

---
+++ src/GeometricCalculator/CalculatorSquare.cs
@@ -0,0 +1,16 @@
+﻿namespace GeometricCalculator;
+
+/// <summary>
+/// Calculation of the square of figures
+/// </summary>
+public struct CalculatorSquare
+{
+    /// <summary>
+    /// Unified calculation of the square of figures
+    /// </summary>
+    /// <typeparam name="T">Type geometric figure</typeparam>
+    /// <param name="figure">Geometric figure</param>
+    /// <param name="square">Implementation square calculation</param>
+    /// <returns>Square</returns>
+    public static double Calculate<T>(T figure, ISquare<T> square) => square.Square(figure);
+}
